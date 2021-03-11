106269328949463440f3714b20b28e6a67705e1a
Pooya Parsa <pyapar@gmail.com>
2021-03-11 22:32:55 +0100

    test: write http driver tests using storage server

--- test/drivers/http.test.ts
+++ test/drivers/http.test.ts
@@ -1,22 +1,21 @@
 import driver from '../../src/drivers/http'
 import { createStorage } from '../../src'
+import { createStorageServer } from '../../src/server'
 import { listen } from 'listhen'
 
 describe('drivers: http', () => {
   it('basic', async () => {
-    const { url, close } = await listen((req, res) => {
-      if (req.url!.includes('404')) {
-        res.writeHead(404)
-      }
-      res.end(req.url!)
-    })
-
     const storage = createStorage()
+    const server = createStorageServer(storage)
+
+    const { url, close } = await listen(server.handle)
     await storage.mount('/http', driver({ base: url }))
 
-    expect(await storage.getItem('http:foo')).toBe('/foo')
-    expect(await storage.hasItem('/http/any')).toBe(true)
-    expect(await storage.hasItem('/http/404')).toBe(false)
+    expect(await storage.hasItem('/http/foo')).toBe(false)
+
+    await storage.setItem('/http/foo', 'bar')
+    expect(await storage.getItem('http:foo')).toBe('bar')
+    expect(await storage.hasItem('/http/foo')).toBe(true)
 
     await close()
   })

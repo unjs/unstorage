a53a1700ecd3c7b72d44b469e646eebc7f772d45
Farnabaz <farnabaz@gmail.com>
2021-10-29 19:21:57 +0330

    fix(build): use cjs extension for drivers

--- build.config.ts
+++ build.config.ts
@@ -6,6 +6,6 @@ export default defineBuildConfig({
     'src/index',
     'src/server',
     { input: 'src/drivers/', outDir: 'drivers', format: 'esm' },
-    { input: 'src/drivers/', outDir: 'drivers', format: 'cjs', declaration: false }
+    { input: 'src/drivers/', outDir: 'drivers', format: 'cjs', ext: 'cjs', declaration: false }
   ]
 })

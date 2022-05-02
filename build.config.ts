4f2a211a1497afc34c8bd19183ae065ecac3d51c
Pooya Parsa <pyapar@gmail.com>
2022-05-02 15:38:26 +0200

    build!: use `./dist` for all subpath exports

--- build.config.ts
+++ build.config.ts
@@ -2,10 +2,13 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   declaration: true,
+  rollup: {
+    emitCJS: true
+  },
   entries: [
     'src/index',
     'src/server',
-    { input: 'src/drivers/', outDir: 'drivers', format: 'esm' },
-    { input: 'src/drivers/', outDir: 'drivers', format: 'cjs', ext: 'cjs', declaration: false }
+    { input: 'src/drivers/', outDir: 'dist/drivers', format: 'esm' },
+    { input: 'src/drivers/', outDir: 'dist/drivers', format: 'cjs', ext: 'cjs', declaration: false }
   ]
 })

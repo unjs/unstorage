be34d5e71861162321103dd57ba6767e8bad9e4d
Pooya Parsa <pyapar@gmail.com>
2022-06-13 13:32:35 +0200

    feat: expose `builtinDrivers`

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,15 @@ export * from './storage'
 export * from './types'
 export * from './utils'
 export { defineDriver } from './drivers/utils'
+
+export const builtinDrivers = {
+  cloudflareKVHTTP: 'unstorage/drivers/cloudflare-kv-http',
+  cloudflareKVBinding: 'unstorage/drivers/cloudflare-kv',
+  fs: 'unstorage/drivers/fs',
+  github: 'unstorage/drivers/github',
+  http: 'unstorage/drivers/http',
+  localStorage: 'unstorage/drivers/localstorage',
+  memory: 'unstorage/drivers/memory',
+  overlay: 'unstorage/drivers/overlay',
+  redis: 'unstorage/drivers/redis'
+}

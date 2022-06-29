f6a941c59443340c1bd3101ab7f842e01abe5bcb
Pooya Parsa <pyapar@gmail.com>
2022-06-29 18:08:04 +0200

    feat: export `BuiltinDriverName` type and kebab-case names

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,16 @@ export { defineDriver } from './drivers/utils'
 export const builtinDrivers = {
   cloudflareKVHTTP: 'unstorage/drivers/cloudflare-kv-http',
   cloudflareKVBinding: 'unstorage/drivers/cloudflare-kv',
+  'cloudflare-kv-http': 'unstorage/drivers/cloudflare-kv-http',
+  'cloudflare-kv-binding': 'unstorage/drivers/cloudflare-kv',
   fs: 'unstorage/drivers/fs',
   github: 'unstorage/drivers/github',
   http: 'unstorage/drivers/http',
   localStorage: 'unstorage/drivers/localstorage',
+  localstorage: 'unstorage/drivers/localstorage',
   memory: 'unstorage/drivers/memory',
   overlay: 'unstorage/drivers/overlay',
   redis: 'unstorage/drivers/redis'
 }
+
+export type BuiltinDriverName = keyof typeof builtinDrivers

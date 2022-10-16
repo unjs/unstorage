7ffb38f742bb09c831359ed0f7ae1e7f703b1ed3
Pooya Parsa <pooya@pi0.io>
2022-10-16 12:40:40 +0200

    chore: update h3 to 0.8.0 and other dependencies to latest

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { RequestListener } from 'http'
-import { createApp, createError, useBody } from 'h3'
+import { createApp, createError, readBody, eventHandler, toNodeListener } from 'h3'
 import { Storage } from './types'
 import { stringify } from './_utils'
 
@@ -11,46 +11,46 @@ export interface StorageServer {
 
 export function createStorageServer (storage: Storage, _opts: StorageServerOptions = {}): StorageServer {
   const app = createApp()
-  app.use(async (req, res) => {
+  app.use(eventHandler(async (event) => {
     // GET => getItem
-    if (req.method === 'GET') {
-      const val = await storage.getItem(req.url!)
+    if (event.req.method === 'GET') {
+      const val = await storage.getItem(event.req.url!)
       if (!val) {
-        const keys = await storage.getKeys(req.url)
+        const keys = await storage.getKeys(event.req.url)
         return keys.map(key => key.replace(/:/g, '/'))
       }
       return stringify(val)
     }
     // HEAD => hasItem + meta (mtime)
-    if (req.method === 'HEAD') {
-      const _hasItem = await storage.hasItem(req.url!)
-      res.statusCode = _hasItem ? 200 : 404
+    if (event.req.method === 'HEAD') {
+      const _hasItem = await storage.hasItem(event.req.url!)
+      event.res.statusCode = _hasItem ? 200 : 404
       if (_hasItem) {
-        const meta = await storage.getMeta(req.url!)
+        const meta = await storage.getMeta(event.req.url!)
         if (meta.mtime) {
-          res.setHeader('Last-Modified', new Date(meta.mtime).toUTCString())
+          event.res.setHeader('Last-Modified', new Date(meta.mtime).toUTCString())
         }
       }
       return ''
     }
     // PUT => setItem
-    if (req.method === 'PUT') {
-      const val = await useBody(req)
-      await storage.setItem(req.url!, val)
+    if (event.req.method === 'PUT') {
+      const val = await readBody(event)
+      await storage.setItem(event.req.url!, val)
       return 'OK'
     }
     // DELETE => removeItem
-    if (req.method === 'DELETE') {
-      await storage.removeItem(req.url!)
+    if (event.req.method === 'DELETE') {
+      await storage.removeItem(event.req.url!)
       return 'OK'
     }
     throw createError({
       statusCode: 405,
       statusMessage: 'Method Not Allowed'
     })
-  })
+  }))
 
   return {
-    handle: app
+    handle: toNodeListener(app)
   }
 }

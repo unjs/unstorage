c726119c616e82f419d3f56350d7e9b2002ac603
Daniel Roe <daniel@roe.dev>
2022-12-18 22:55:04 +0000

    build: provide backwards-compatible type entries

--- build.config.ts
+++ build.config.ts
@@ -1,3 +1,6 @@
+import { mkdir, writeFile } from "node:fs/promises";
+import { dirname, join, relative } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
@@ -10,5 +13,19 @@ export default defineBuildConfig({
     "src/server",
     { input: "src/drivers/", outDir: "dist/drivers", format: "esm" },
     { input: "src/drivers/", outDir: "dist/drivers", format: "cjs", ext: "cjs", declaration: false }
-  ]
+  ],
+  hooks: {
+    async "build:done" (ctx) {
+      for (const entry of ctx.buildEntries) {
+        if (/^drivers\/.*\.d\.ts$/.test(entry.path)) {
+          const target = fileURLToPath(new URL(join("dist", entry.path), import.meta.url))
+          const declaration = fileURLToPath(new URL(entry.path, import.meta.url));
+          const relativePath = relative(dirname(declaration), target);
+
+          await mkdir(dirname(declaration), { recursive: true });
+          await writeFile(declaration, `export * from "${relativePath.slice(0, -5)}";`);
+        }
+      }
+    }
+  }
 });

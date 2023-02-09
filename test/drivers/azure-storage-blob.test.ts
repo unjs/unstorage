9b3dc10fa6c97432d29e455b10f855917d0b8be9
Jan-Henrik Damaschke <jdamaschke@visorian.com>
2023-02-09 03:25:56 +0100

    test(azure-storage-blob):  Updated tests to run azurite only if needed

--- test/drivers/azure-storage-blob.test.ts
+++ test/drivers/azure-storage-blob.test.ts
@@ -2,18 +2,25 @@ import { describe, it, expect, beforeAll, afterAll } from "vitest";
 import driver from "../../src/drivers/azure-storage-blob";
 import { testDriver } from "./utils";
 import { BlobServiceClient } from "@azure/storage-blob";
+import { ChildProcess, exec } from "child_process";
 
 describe("drivers: azure-storage-blob", () => {
+  let azuriteProcess: ChildProcess;
   beforeAll(async () => {
+    azuriteProcess = exec("npm run azurite-blob-storage");
     const client = BlobServiceClient.fromConnectionString(
       "UseDevelopmentStorage=true"
     );
-    await client.createContainer("unstorage");
+    const containerClient = client.getContainerClient("unstorage");
+    await containerClient.createIfNotExists();
   });
   testDriver({
     driver: driver({
       connectionString: "UseDevelopmentStorage=true",
       accountName: "local",
     }),
+  });
+  afterAll(() => {
+    azuriteProcess.kill(9);
   });
 });

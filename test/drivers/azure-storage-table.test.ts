76fc831dabe9cc0cb447320b7d0761be8bc021ee
Jan-Henrik Damaschke <jdamaschke@visorian.com>
2023-02-09 03:27:45 +0100

    test(azure-storage-table):  Removed unneeded sleep

--- test/drivers/azure-storage-table.test.ts
+++ test/drivers/azure-storage-table.test.ts
@@ -3,16 +3,11 @@ import driver from "../../src/drivers/azure-storage-table";
 import { testDriver } from "./utils";
 import { TableClient } from "@azure/data-tables";
 import { ChildProcess, exec } from "child_process";
-import { promisify } from "util";
-
-const sleep = promisify(setTimeout);
 
 describe("drivers: azure-storage-table", () => {
   let azuriteProcess: ChildProcess;
   beforeAll(async () => {
     azuriteProcess = exec("npm run azurite-table-storage");
-    // Wait for Azurite to start
-    sleep(1000);
     const client = TableClient.fromConnectionString(
       "UseDevelopmentStorage=true",
       "unstorage"
@@ -26,6 +21,6 @@ describe("drivers: azure-storage-table", () => {
     }),
   });
   afterAll(() => {
-    azuriteProcess.kill();
+    azuriteProcess.kill(9);
   });
 });

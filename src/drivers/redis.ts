7a70c2982542ef23ea0b9d14a2ea6b7173dc382b
Pooya Parsa <pooya@pi0.io>
2023-02-27 21:20:47 +0100

    small updates

--- src/drivers/redis.ts
+++ src/drivers/redis.ts
@@ -11,14 +11,17 @@ export interface RedisOptions extends _RedisOptions {
    * Optional prefix to use for all keys. Can be used for namespacing.
    */
   base?: string;
+
   /**
    * Url to use for connecting to redis. Takes precedence over `host` option. Has the format `redis://<REDIS_USER>:<REDIS_PASSWORD>@<REDIS_HOST>:<REDIS_PORT>`
    */
   url?: string;
+
   /**
    * List of redis nodes to use for cluster mode. Takes precedence over `url` and `host` options.
    */
   cluster?: ClusterNode[];
+
   /**
    * Options to use for cluster mode.
    */
@@ -26,29 +29,28 @@ export interface RedisOptions extends _RedisOptions {
 }
 
 export default defineDriver((opts: RedisOptions = {}) => {
-  if (!opts.url && !opts.host && !opts.cluster)
-    throw new Error(
-      "Redis driver requires either a host, url or cluster option"
-    );
-  const _opts = { lazyConnect: true, ...opts };
-  const { cluster, clusterOptions = {}, url, base, ...redisOptions } = _opts;
-
   let redisClient: Redis | Cluster;
   const getRedisClient = () => {
-    if (!redisClient) {
-      if (cluster) {
-        redisClient = new Redis.Cluster(cluster, clusterOptions);
-      } else if (url) {
-        redisClient = new Redis(url, redisOptions);
-      } else {
-        redisClient = new Redis(redisOptions);
-      }
+    if (redisClient) {
+      return redisClient;
+    }
+    if (!opts.url && !opts.cluster) {
+      throw new Error(
+        "Redis driver requires either a host, url or cluster option"
+      );
+    }
+    if (opts.cluster) {
+      redisClient = new Redis.Cluster(opts.cluster, opts.clusterOptions);
+    } else if (opts.url) {
+      redisClient = new Redis(opts.url, opts);
+    } else {
+      redisClient = new Redis(opts);
     }
     return redisClient;
   };
 
-  const p = (key: string) => (base ? `${base}:${key}` : key); // Prefix a key. Uses base for backwards compatibility
-  const d = (key: string) => (base ? key.replace(base, "") : key); // Deprefix a key
+  const p = (key: string) => (opts.base ? `${opts.base}:${key}` : key); // Prefix a key. Uses base for backwards compatibility
+  const d = (key: string) => (opts.base ? key.replace(opts.base, "") : key); // Deprefix a key
 
   return {
     async hasItem(key) {

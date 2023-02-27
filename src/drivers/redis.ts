5db23b8d8e4c3464d840698bd5bf64ac26f55bce
Jan-Henrik Damaschke <jdamaschke@outlook.de>
2023-02-27 21:22:14 +0100

    refactor(redis): driver improvements (#160)

--- src/drivers/redis.ts
+++ src/drivers/redis.ts
@@ -1,46 +1,86 @@
 import { defineDriver } from "./utils";
-import Redis, { RedisOptions as _RedisOptions } from "ioredis";
+import Redis, {
+  Cluster,
+  ClusterNode,
+  ClusterOptions,
+  RedisOptions as _RedisOptions,
+} from "ioredis";
 
 export interface RedisOptions extends _RedisOptions {
-  base: string;
-  url: string;
+  /**
+   * Optional prefix to use for all keys. Can be used for namespacing.
+   */
+  base?: string;
+
+  /**
+   * Url to use for connecting to redis. Takes precedence over `host` option. Has the format `redis://<REDIS_USER>:<REDIS_PASSWORD>@<REDIS_HOST>:<REDIS_PORT>`
+   */
+  url?: string;
+
+  /**
+   * List of redis nodes to use for cluster mode. Takes precedence over `url` and `host` options.
+   */
+  cluster?: ClusterNode[];
+
+  /**
+   * Options to use for cluster mode.
+   */
+  clusterOptions?: ClusterOptions;
 }
 
-export default defineDriver<RedisOptions>((_opts) => {
-  const opts = { lazyConnect: true, ..._opts };
-  const redis = opts.url ? new Redis(opts?.url, opts) : new Redis(opts);
+export default defineDriver((opts: RedisOptions = {}) => {
+  let redisClient: Redis | Cluster;
+  const getRedisClient = () => {
+    if (redisClient) {
+      return redisClient;
+    }
+    if (!opts.url && !opts.cluster) {
+      throw new Error(
+        "Redis driver requires either a host, url or cluster option"
+      );
+    }
+    if (opts.cluster) {
+      redisClient = new Redis.Cluster(opts.cluster, opts.clusterOptions);
+    } else if (opts.url) {
+      redisClient = new Redis(opts.url, opts);
+    } else {
+      redisClient = new Redis(opts);
+    }
+    return redisClient;
+  };
 
-  let base = opts?.base || "";
-  if (base && !base.endsWith(":")) {
-    base += ":";
-  }
-  const r = (key: string) => base + key;
+  const p = (key: string) => (opts.base ? `${opts.base}:${key}` : key); // Prefix a key. Uses base for backwards compatibility
+  const d = (key: string) => (opts.base ? key.replace(opts.base, "") : key); // Deprefix a key
 
   return {
-    hasItem(key) {
-      return redis.exists(r(key)).then(Boolean);
+    async hasItem(key) {
+      return Boolean(await getRedisClient().exists(p(key)));
     },
-    getItem(key) {
-      return redis.get(r(key));
+    async getItem(key) {
+      const value = await getRedisClient().get(p(key));
+      return value === null ? null : value;
     },
-    setItem(key, value) {
-      return redis.set(r(key), value).then(() => {});
+    async setItem(key, value) {
+      await getRedisClient().set(p(key), value);
     },
-    removeItem(key) {
-      return redis.del(r(key)).then(() => {});
+    async removeItem(key) {
+      await getRedisClient().del(p(key));
     },
-    getKeys() {
-      return redis.keys(r("*"));
+    async getKeys() {
+      const keys: string[] = await getRedisClient().keys(p("*"));
+      return keys.map((key) => d(key));
     },
     async clear() {
-      const keys = await redis.keys(r("*"));
+      const keys = await getRedisClient().keys(p("*"));
       if (keys.length === 0) {
         return;
       }
-      return redis.del(keys).then(() => {});
+      return getRedisClient()
+        .del(keys)
+        .then(() => {});
     },
     dispose() {
-      return redis.disconnect();
+      return getRedisClient().disconnect();
     },
   };
 });

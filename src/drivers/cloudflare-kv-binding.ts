233d434e61f655c93ee2849d92852a72d2142a71
Pooya Parsa <pooya@pi0.io>
2023-04-16 15:20:12 +0200

    lint

--- src/drivers/cloudflare-kv-binding.ts
+++ src/drivers/cloudflare-kv-binding.ts
@@ -47,10 +47,8 @@ function getBinding(binding: KVNamespace | string = "STORAGE") {
 
   if (typeof binding === "string") {
     bindingName = binding;
-    binding = (
-      globalThis[bindingName] ||
-      globalThis.__env__?.[bindingName]
-    ) as KVNamespace;
+    binding = (globalThis[bindingName] ||
+      globalThis.__env__?.[bindingName]) as KVNamespace;
   }
 
   if (!binding) {

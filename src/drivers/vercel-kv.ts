c8e12deb8d9c01cb1214dfb22e4d05da236a305f
Pooya Parsa <pooya@pi0.io>
2023-05-05 07:42:38 +0200

    update driver

--- src/drivers/vercel-kv.ts
+++ src/drivers/vercel-kv.ts
@@ -4,49 +4,48 @@ import type { RedisConfigNodejs } from "@upstash/redis";
 
 import { defineDriver, normalizeKey, joinKeys } from "./utils";
 
-export interface RedisOptions extends RedisConfigNodejs {
+export interface VercelKVOptions extends RedisConfigNodejs {
   base?: string;
 }
 
-export default defineDriver<RedisOptions>((opts) => {
+export default defineDriver<VercelKVOptions>((opts) => {
   const base = normalizeKey(opts?.base);
   const r = (...keys: string[]) => joinKeys(base, ...keys);
 
-  let _connection: VercelKV;
-  const getConnection = () => {
-    if (!_connection) {
-      // `connect` configures a connection class rather than initiating a connection
-      _connection = createClient(opts);
+  let _client: VercelKV;
+  const getClient = () => {
+    if (!_client  ) {
+      _client = createClient(opts);
     }
-    return _connection;
+    return _client;
   };
 
   return {
     hasItem(key) {
-      return getConnection().exists(r(key)).then(Boolean);
+      return getClient().exists(r(key)).then(Boolean);
     },
     getItem(key) {
-      return getConnection().get(r(key));
+      return getClient().get(r(key));
     },
     setItem(key, value) {
-      return getConnection()
+      return getClient()
         .set(r(key), value)
         .then(() => {});
     },
     removeItem(key) {
-      return getConnection()
+      return getClient()
         .del(r(key))
         .then(() => {});
     },
     getKeys(base) {
-      return getConnection().keys(r(base, "*"));
+      return getClient().keys(r(base, "*"));
     },
     async clear(base) {
-      const keys = await getConnection().keys(r(base, "*"));
+      const keys = await getClient().keys(r(base, "*"));
       if (keys.length === 0) {
         return;
       }
-      return getConnection()
+      return getClient()
         .del(...keys)
         .then(() => {});
     },

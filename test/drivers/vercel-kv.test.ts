329496cff8964295b3d2ebe85c0462110b867f94
Pooya Parsa <pooya@pi0.io>
2023-05-05 10:07:50 +0200

    test: fix `vercel-kv` test

--- test/drivers/vercel-kv.test.ts
+++ test/drivers/vercel-kv.test.ts
@@ -1,13 +1,23 @@
-import { describe } from "vitest";
-import driver from "../../src/drivers/vercel-kv";
+import { describe, it } from "vitest";
+// import driver from "../../src/drivers/vercel-kv";
 import { testDriver } from "./utils";
 
 // TODO: Only works locally with env. Mock upstash client to run in CI
 
 const hasEnv = process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN;
 
-describe.skipIf(!hasEnv)("drivers: vercel-kv", () => {
-  testDriver({
-    driver: driver({}),
+if (hasEnv) {
+  describe("drivers: vercel-kv", async () => {
+    const driver = await import("../../src/drivers/vercel-kv").then(
+      (r) => r.default
+    );
+    testDriver({
+      driver: driver({}),
+    });
   });
-});
+} else {
+  // TODO: vitest describe.skipIf has no effect!!
+  describe("drivers: vercel-kv", () => {
+    it.skip("", () => {});
+  });
+}

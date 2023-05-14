85af183ac19c735eb67e2ca234ba549ad0ac330a
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2023-05-14 10:13:11 -0300

    feat(drivers): add idb-keyval driver

---
+++ src/drivers/idb-keyval.ts
@@ -0,0 +1,43 @@
+import { defineDriver } from './utils'
+import { get, set, clear, del, keys, createStore, type UseStore } from 'idb-keyval'
+
+export interface IDBKeyvalOptions {
+    dbName?: string
+    storeName?: string
+    base?: string
+}
+
+const DRIVER_NAME = "idb-keyval";
+
+export default defineDriver((opts: IDBKeyvalOptions = {}) => {
+    let customStore: UseStore | undefined
+
+    if (opts.dbName && opts.storeName) {
+        customStore = createStore(opts.dbName, opts.storeName)
+    }
+
+    const r = (key: string) => (opts.base ? opts.base + ":" : "") + key;
+
+    return {
+        name: DRIVER_NAME,
+        options: opts,
+        hasItem(key) {
+            return get(r(key), customStore).then((item) => item !== undefined)
+        },
+        getItem(key) {
+            return get(r(key), customStore)
+        },
+        setItem(key, value) {
+            return set(r(key), value, customStore)
+        },
+        removeItem(key) {
+            return del(r(key), customStore)
+        },
+        getKeys() {
+            return keys(customStore)
+        },
+        clear() {
+            return clear(customStore)
+        },
+    }
+})

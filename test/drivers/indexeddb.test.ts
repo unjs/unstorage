9d6a36a35d85791142d3d44c9f43a7be36d98737
Hebilicious <xsh4k3@gmail.com>
2023-07-03 05:37:25 +0700

    test: add customStore tests

--- test/drivers/indexeddb.test.ts
+++ test/drivers/indexeddb.test.ts
@@ -1,10 +1,28 @@
-import { describe } from "vitest";
+import { del } from "idb-keyval";
+import { describe, expect, it } from "vitest";
 import driver from "../../src/drivers/indexeddb";
 import { testDriver } from "./utils";
 import "fake-indexeddb/auto";
+import { createStorage } from "../../src";
 
 describe("drivers: indexeddb", () => {
-  testDriver({
-    driver: driver({ dbName: "test-db" }),
+  testDriver({ driver: driver({ dbName: "test-db" }) });
+
+  const customStorage = createStorage({
+    driver: driver({
+      dbName: "custom-db",
+      storeName: "custom-name",
+      base: "unstorage",
+    }),
+  });
+
+  it("can use a customStore", async () => {
+    await customStorage.setItem("first", "foo");
+    await customStorage.setItem("second", "bar");
+    expect(await customStorage.getItem("first")).toBe("foo");
+    await customStorage.removeItem("first");
+    expect(await customStorage.getKeys()).toMatchObject(["unstorage:second"]);
+    await customStorage.clear();
+    expect(await customStorage.hasItem("second")).toBe(false);
   });
 });

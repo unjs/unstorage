d554d59d46bf3d4dd891f139af055ec8fcdff4bd
yarrow.love <creator@shareable.vision>
2023-07-28 19:45:29 -0400

    Optimize: Leverage 'etag' to validate cache.

--- src/drivers/webdav.ts
+++ src/drivers/webdav.ts
@@ -26,43 +26,218 @@ export interface WebdavFile {
   body?: string;
   meta: {
     href: string;
+    etag: string;
     type?: string;
     size?: number;
     atime?: Date;
     mtime?: Date;
-    etag?: string;
   };
 }
 
 export default defineDriver<Partial<WebdavDriverOptions>>((config) => {
   const options = Object.assign(defaultOptions, config) as WebdavDriverOptions;
-  const client = createWebdavClient(options);
-  client.isConnected().then((ok) => {
+
+  const source = (() => {
+    try {
+      return new URL(
+        options.source.endsWith("/") ? options.source : options.source + "/"
+      );
+    } catch {
+      throw errorMessage("'source' must be a valid URL.");
+    }
+  })();
+
+  const headers = { ...options.headers } as { [key: string]: string };
+  if (options.username && options.password) {
+    headers.Authorization = `Basic ${encodeBase64(
+      `${options.username}:${options.password}`
+    )}`;
+  }
+
+  const isConnected = async () => {
+    try {
+      const response = await fetch(options.source, {
+        method: "PROPFIND",
+        headers: {
+          ...headers,
+          Depth: "0",
+        },
+      });
+      return Boolean(response.status === 207);
+    } catch {
+      return false;
+    }
+  };
+  isConnected().then((ok) => {
     if (!ok)
       throw errorMessage(`Failed to connect to source: '${options.source}'`);
   });
 
-  let files: Record<string, WebdavFile> = {};
-  let lastCheck = 0;
-  let syncPromise: undefined | Promise<any>;
+  const xmlParser = new XMLParser();
+  const fetchXML = async (uri: string) => {
+    try {
+      const response = await fetch(uri, {
+        method: "PROPFIND",
+        headers: {
+          ...headers,
+          Depth: options.infinityDepthHeaderUnavailable ? "1" : "infinity",
+        },
+        body: `<?xml version="1.0"?>
+          <d:propfind  xmlns:d="DAV:">
+          <d:prop>
+              <d:resourcetype />
+              <d:getcontenttype />
+              <d:getlastmodified />
+              <d:getcontentlength />
+              <d:getetag />
+          </d:prop>
+          </d:propfind>`,
+      });
 
-  const syncFiles = async () => {
-    const fetchFiles = async () => {
-      try {
-        return await client.fetchFiles();
-      } catch {
-        console.error(errorMessage("Failed to fetch source."));
+      return response.status === 207
+        ? xmlParser.parse(await response.text())
+        : {};
+    } catch {
+      return {};
+    }
+  };
+
+  const latest = {
+    etag: "",
+    atime: 0,
+  };
+
+  const files: Record<string, WebdavFile> = {};
+  const fetchFiles = async () => {
+    const fetchDirectory = async (uri: string) => {
+      const xml = await fetchXML(uri);
+      const key = (() => {
+        const keyPrefix = Object.keys(xml)
+          .find((key) => key.endsWith("multistatus"))
+          ?.split(":")[0];
+        return (key: string) => (keyPrefix ? `${keyPrefix}:${key}` : key);
+      })();
+
+      const dirents = xml[key("multistatus")]?.[key("response")] || [];
+      const subdirectories: Array<Promise<any>> = [];
+      for (const dirent of dirents) {
+        // http://www.webdav.org/specs/rfc4918.html#ELEMENT_href
+        const href = (dirent[key("href")] as string) || undefined;
+        if (!href) continue;
+
+        // http://www.webdav.org/specs/rfc4918.html#ELEMENT_status
+        const getStatus = (propstat: any) => {
+          const status = (propstat[key("status")] as string | undefined)?.split(
+            " "
+          );
+          if (status) {
+            const [protocol, code, message] = status;
+            return validInteger(code);
+          }
+        };
+
+        // http://www.webdav.org/specs/rfc4918.html#ELEMENT_propstat
+        const properties = (() => {
+          const p = dirent[key("propstat")];
+          const propstats = Array.isArray(p) ? p : [p];
+          const propstat = propstats.find((propstat) =>
+            Boolean(getStatus(propstat) === 200)
+          );
+          if (propstat) return propstat[key("prop")];
+        })();
+        if (!properties) continue;
+
+        // http://www.webdav.org/specs/rfc4918.html#PROPERTY_getetag
+        const etag = (properties[key("getetag")] as string)
+          ?.match(/[^"]+/)
+          ?.shift();
+        if (!etag) continue;
+
+        if (href === source.pathname) {
+          if (etag === latest.etag) return;
+          else latest.etag = etag;
+        }
+
+        // http://www.webdav.org/specs/rfc4918.html#PROPERTY_resourcetype
+        const isCollection = Boolean(
+          typeof properties[key("resourcetype")] === "object" &&
+            key("collection") in properties[key("resourcetype")]
+        );
+
+        if (isCollection) {
+          const subdirectory = joinURL(source.origin, href);
+          if (options.infinityDepthHeaderUnavailable && subdirectory !== uri) {
+            subdirectories.push(fetchDirectory(subdirectory));
+          }
+          continue;
+        }
+
+        const record = decodeURI(href)
+          .replace(source.pathname, "")
+          .replace(/\//g, ":");
+
+        if (record in files) {
+          if (files[record].meta.etag === etag) continue;
+          files[record].body = undefined;
+        }
+
+        if (record in files && files[record].meta.etag === etag) continue;
+
+        files[record] = {
+          meta: {
+            href,
+            etag,
+
+            // http://www.webdav.org/specs/rfc4918.html#PROPERTY_getcontenttype
+            type: (properties[key("getcontenttype")] as string) || undefined,
+
+            // http://www.webdav.org/specs/rfc4918.html#PROPERTY_getcontentlength
+            size: validInteger(properties[key("getcontentlength")]),
+
+            // http://www.webdav.org/specs/rfc4918.html#PROPERTY_getlastmodified
+            mtime: validDate(properties[key("getlastmodified")]),
+          },
+        };
       }
+      if (options.infinityDepthHeaderUnavailable)
+        await Promise.all(subdirectories);
     };
 
-    if (lastCheck + options.ttl! * 1000 > Date.now()) {
+    await fetchDirectory(source.href);
+    latest.atime = Date.now();
+  };
+
+  const fetchFile = async (href: string) => {
+    const uri = joinURL(source.origin, href);
+    try {
+      const response = await fetch(uri, { headers });
+      const getHeader = (key: string) => {
+        const value = response.headers.get(key);
+        if (value) return value;
+      };
+
+      return {
+        body: await response.text(),
+        meta: {
+          href,
+          etag: getHeader("etag"),
+          type: getHeader("Content-Type"),
+          size: validInteger(getHeader("Content-Length")),
+          atime: validDate(getHeader("Date")),
+          mtime: validDate(getHeader("Last-Modified")),
+        },
+      } as WebdavFile;
+    } catch {
       return;
     }
+  };
+
+  let syncPromise: undefined | Promise<any>;
+  const syncFiles = async () => {
+    if (options.ttl && Date.now() < latest.atime + options.ttl * 1000) return;
 
     if (!syncPromise) syncPromise = fetchFiles();
-    files = await syncPromise;
-
-    lastCheck = Date.now();
+    await syncPromise;
     syncPromise = undefined;
   };
 
@@ -81,13 +256,10 @@ export default defineDriver<Partial<WebdavDriverOptions>>((config) => {
       await syncFiles();
       if (!(key in files)) return null;
 
-      //* If body does exist, must be re-validated. (Zero-depth request for etag?)
       if (!files[key]?.body) {
-        // should re-validate against etag ("0"-Depth PROPFIND on file)?
-
         const href = files[key].meta.href;
-        const response = await client.fetchFile(href);
-        if (response.ok) files[key] = response.file as WebdavFile;
+        const file = await fetchFile(href);
+        if (file) files[key] = file;
       }
       return files[key].body;
     },
@@ -97,214 +269,6 @@ export default defineDriver<Partial<WebdavDriverOptions>>((config) => {
     },
   };
 });
-
-function createWebdavClient(options: {
-  source: string;
-  username?: string;
-  password?: string;
-  headers?: { [key: string]: string };
-
-  infinityDepthHeaderUnavailable?: boolean;
-}) {
-  const source = (() => {
-    try {
-      return new URL(
-        options.source.endsWith("/") ? options.source : options.source + "/"
-      );
-    } catch {
-      throw errorMessage("'source' must be a valid URL.");
-    }
-  })();
-
-  const headers = { ...options.headers } as { [key: string]: string };
-  if (options.username && options.password) {
-    headers.Authorization = `Basic ${encodeBase64(
-      `${options.username}:${options.password}`
-    )}`;
-  }
-
-  const xmlParser = new XMLParser();
-  const fetchXML = async (source: string) => {
-    const body = `<?xml version="1.0"?>
-            <d:propfind  xmlns:d="DAV:">
-            <d:prop>
-                <d:resourcetype />
-                <d:getcontenttype />
-                <d:getlastmodified />
-                <d:getcontentlength />
-                <d:getetag />
-            </d:prop>
-            </d:propfind>`;
-
-    try {
-      const response = await fetch(source, {
-        method: "PROPFIND",
-        headers: {
-          ...headers,
-          Depth: options.infinityDepthHeaderUnavailable ? "1" : "infinity",
-        },
-        body,
-      });
-
-      return response.status === 207
-        ? xmlParser.parse(await response.text())
-        : {};
-    } catch {
-      return {};
-    }
-  };
-
-  return {
-    async isConnected() {
-      try {
-        const response = await fetch(options.source, {
-          method: "PROPFIND",
-          headers: {
-            ...headers,
-            Depth: "0",
-          },
-        });
-        return Boolean(response.status === 207);
-      } catch {
-        return false;
-      }
-    },
-
-    async fetchFiles() {
-      const files: Record<string, WebdavFile> = {};
-      const fetchDirectory = async (url: string) => {
-        const xml = await fetchXML(url);
-        const key = (() => {
-          const keyPrefix = Object.keys(xml)
-            .find((key) => key.endsWith("multistatus"))
-            ?.split(":")[0];
-          return (key: string) => (keyPrefix ? `${keyPrefix}:${key}` : key);
-        })();
-
-        const dirents = xml[key("multistatus")]?.[key("response")] || [];
-        const subdirectories: Array<Promise<any>> = [];
-        for (const dirent of dirents) {
-          // http://www.webdav.org/specs/rfc4918.html#ELEMENT_status
-          const getStatus = (propstat: any) => {
-            const status = (
-              propstat[key("status")] as string | undefined
-            )?.split(" ");
-            if (status) {
-              const [protocol, code, message] = status;
-              return code ? parseInt(code) : undefined;
-            }
-          };
-
-          // http://www.webdav.org/specs/rfc4918.html#ELEMENT_propstat
-          const getProperties = () => {
-            const p = dirent[key("propstat")];
-            const propstats = Array.isArray(p) ? p : [p];
-            const propstat = propstats.find((propstat) =>
-              Boolean(getStatus(propstat) === 200)
-            );
-            if (propstat) return propstat[key("prop")];
-          };
-
-          const properties = getProperties();
-          if (!properties) continue;
-
-          // http://www.webdav.org/specs/rfc4918.html#ELEMENT_href
-          const href = (dirent[key("href")] as string) || undefined;
-          if (!href) continue;
-
-          // http://www.webdav.org/specs/rfc4918.html#PROPERTY_resourcetype
-          const isCollection = () => {
-            if (typeof properties[key("resourcetype")] !== "object")
-              return false;
-            return Boolean(
-              key("collection") in properties[key("resourcetype")]
-            );
-          };
-          if (isCollection()) {
-            const subdirectory = joinURL(source.origin, href);
-            if (
-              options.infinityDepthHeaderUnavailable &&
-              subdirectory !== url
-            ) {
-              subdirectories.push(fetchDirectory(subdirectory));
-            }
-            continue;
-          }
-
-          // http://www.webdav.org/specs/rfc4918.html#PROPERTY_getcontenttype
-          const getContentType = () =>
-            (properties[key("getcontenttype")] as string) || undefined;
-
-          // http://www.webdav.org/specs/rfc4918.html#PROPERTY_getlastmodified
-          const getLastModified = () =>
-            validDate(properties[key("getlastmodified")]);
-
-          // http://www.webdav.org/specs/rfc4918.html#PROPERTY_getcontentlength
-          const getContentLength = () =>
-            validInteger(properties[key("getcontentlength")]);
-
-          // http://www.webdav.org/specs/rfc4918.html#PROPERTY_getetag
-          const getETag = () =>
-            (properties[key("getetag")] as string)?.match(/[^"]+/)?.shift();
-
-          const record = decodeURI(href)
-            .replace(source.pathname, "")
-            .replace(/\//g, ":");
-
-          files[record] = {
-            meta: {
-              href,
-              type: getContentType(),
-              size: getContentLength(),
-              mtime: getLastModified(),
-              etag: getETag(),
-            },
-          };
-        }
-        if (options.infinityDepthHeaderUnavailable)
-          await Promise.all(subdirectories);
-      };
-
-      await fetchDirectory(options.source);
-      return files;
-    },
-
-    async fetchFile(href: string): Promise<{
-      ok: boolean;
-      file?: WebdavFile;
-      error?: any;
-    }> {
-      const url = joinURL(source.origin, href);
-      try {
-        const response = await fetch(url, { headers });
-        const getHeader = (key: string) => {
-          const value = response.headers.get(key);
-          return value ? value : undefined;
-        };
-
-        return {
-          ok: true,
-          file: {
-            body: await response.text(),
-            meta: {
-              href,
-              type: getHeader("Content-Type"),
-              size: validInteger(getHeader("Content-Length")),
-              atime: validDate(getHeader("Date")),
-              mtime: validDate(getHeader("Last-Modified")),
-              etag: getHeader("etag"),
-            },
-          } as WebdavFile,
-        };
-      } catch (error) {
-        return {
-          ok: false,
-          error,
-        };
-      }
-    },
-  };
-}
 
 function encodeBase64(string: string) {
   return Buffer.from(string, "utf8").toString("base64");

ee3c8d5cf1d51da374fdc27ef023b981d5016e8f
Pooya Parsa <pooya@pi0.io>
2023-08-07 22:22:05 +0200

    lint

--- test/drivers/deno-kv.test.ts
+++ test/drivers/deno-kv.test.ts
@@ -1,20 +1,22 @@
 import { describe, it, expect, vi } from "vitest";
 import { execa, execaCommandSync } from "execa";
 import { readFile, mkdir, writeFile } from "fs/promises";
-import crypto from "crypto"
+import crypto from "crypto";
 
 const hasDeno =
   execaCommandSync("deno --version", { stdio: "ignore", reject: false })
     .exitCode === 0;
 
-try{
-  await mkdir("./tmp") // Make tmp dir
-} catch(_e) {} // Dir exists
+try {
+  await mkdir("./tmp"); // Make tmp dir
+} catch (_e) {} // Dir exists
 
 describe.runIf(hasDeno)("drivers: deno-kv", async () => {
-  const driverModuleCode = (await readFile("./src/drivers/deno-kv.ts")).toString().replace(/(?<=from ")\.\/utils\/.*?(?=")/g, str => {
-    return "../src/drivers/"+str+".ts"
-  });
+  const driverModuleCode = (await readFile("./src/drivers/deno-kv.ts"))
+    .toString()
+    .replace(/(?<=from ")\.\/utils\/.*?(?=")/g, (str) => {
+      return "../src/drivers/" + str + ".ts";
+    });
   await writeFile("./tmp/deno-kv-for-deno.ts", driverModuleCode);
 
   const driverInitCode = `
@@ -24,7 +26,7 @@ describe.runIf(hasDeno)("drivers: deno-kv", async () => {
     driver: driver({}),
   });
   `;
-  it("can set and get items", async ()=>{
+  it("can set and get items", async () => {
     const key = crypto.randomUUID(); // Create random key
     const value = crypto.randomUUID(); // Create random value
 
@@ -32,11 +34,15 @@ describe.runIf(hasDeno)("drivers: deno-kv", async () => {
     await storage.setItem("${key}","${value}");
     console.log(await storage.getItem("${key}"));
     `;
-    expect((await execa("deno", [
-      "eval",
+    expect(
+      (
+        await execa("deno", [
+          "eval",
 
-      code,
-      "--unstable", // If Deno use Kv, we must give "--unstable" flag to Deno.
-    ])).stdout).toBe(value)
+          code,
+          "--unstable", // If Deno use Kv, we must give "--unstable" flag to Deno.
+        ])
+      ).stdout
+    ).toBe(value);
   }, 100000);
-})
+});

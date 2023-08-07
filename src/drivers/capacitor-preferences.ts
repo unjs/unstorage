013fbc937748e53e0afdb950972ef4e6cce48772
Pooya Parsa <pooya@pi0.io>
2023-08-08 01:25:20 +0200

    small updates

--- src/drivers/capacitor-preferences.ts
+++ src/drivers/capacitor-preferences.ts
@@ -9,50 +9,41 @@ export interface CapacitorPreferencesOptions {
 }
 
 export default defineDriver<CapacitorPreferencesOptions>((opts) => {
-  const base = normalizeKey(opts?.base);
-  const r = (key: string) => joinKeys(base, key);
-
-  const filterKeysByBase = (base: string) => {
-    if (!base) return Preferences.keys().then(({ keys }) => keys);
-
-    return Preferences.keys().then(({ keys }) =>
-      keys.filter((key) => key.startsWith(base))
-    );
-  };
+  const base = normalizeKey(opts?.base || "");
+  const resolveKey = (key: string) => joinKeys(base, key);
 
   return {
     name: DRIVER_NAME,
     options: opts,
     hasItem(key) {
-      return Preferences.keys().then(({ keys }) => keys.includes(r(key)));
+      return Preferences.keys().then((r) => r.keys.includes(resolveKey(key)));
     },
     getItem(key) {
-      return Preferences.get({ key: r(key) }).then(({ value }) => value);
+      return Preferences.get({ key: resolveKey(key) }).then((r) => r.value);
     },
     getItemRaw(key) {
-      return Preferences.get({ key: r(key) }).then(({ value }) => value);
+      return Preferences.get({ key: resolveKey(key) }).then((r) => r.value);
     },
     setItem(key, value) {
-      return Preferences.set({ key: r(key), value });
+      return Preferences.set({ key: resolveKey(key), value });
     },
     setItemRaw(key, value) {
-      return Preferences.set({ key: r(key), value });
+      return Preferences.set({ key: resolveKey(key), value });
     },
     removeItem(key) {
-      return Preferences.remove({ key: r(key) });
+      return Preferences.remove({ key: resolveKey(key) });
     },
-    getKeys() {
-      const optionsBase = opts?.base ?? "";
-      return filterKeysByBase(optionsBase).then((keys) =>
-        keys.map((key) => key.slice(optionsBase.length))
-      );
+    async getKeys() {
+      const { keys } = await Preferences.keys();
+      return keys.map((key) => key.slice(base.length));
     },
-    clear() {
-      const optionsBase = opts?.base ?? "";
-      return filterKeysByBase(optionsBase).then((keys) =>
-        Promise.all(keys.map((key) => Preferences.remove({ key }))).then(
-          () => void 0
-        )
+    async clear(prefix) {
+      const { keys } = await Preferences.keys();
+      const _prefix = resolveKey(prefix || "");
+      await Promise.all(
+        keys
+          .filter((key) => key.startsWith(_prefix))
+          .map((key) => Preferences.remove({ key }))
       );
     },
   };

01772b30c771dfb312995475cb57030bcb945dac
Hebilicious <xsh4k3@gmail.com>
2023-08-17 19:03:42 +0700

    feat: add redis-upstash

---
+++ src/drivers/redis-upstash.ts
@@ -0,0 +1,96 @@
+import { defineDriver, joinKeys } from "./utils";
+import Redis, {
+  Cluster,
+  ClusterNode,
+  ClusterOptions,
+  RedisOptions as _RedisOptions,
+} from "ioredis";
+
+export interface RedisOptions extends _RedisOptions {
+  /**
+   * Optional prefix to use for all keys. Can be used for namespacing.
+   */
+  base?: string;
+
+  /**
+   * Url to use for connecting to redis. Takes precedence over `host` option. Has the format `redis://<REDIS_USER>:<REDIS_PASSWORD>@<REDIS_HOST>:<REDIS_PORT>`
+   */
+  url?: string;
+
+  /**
+   * List of redis nodes to use for cluster mode. Takes precedence over `url` and `host` options.
+   */
+  cluster?: ClusterNode[];
+
+  /**
+   * Options to use for cluster mode.
+   */
+  clusterOptions?: ClusterOptions;
+
+  /**
+   * Default TTL for all items in seconds.
+   */
+  ttl?: number;
+}
+
+const DRIVER_NAME = "redis";
+
+export default defineDriver((opts: RedisOptions = {}) => {
+  let redisClient: Redis | Cluster;
+  const getRedisClient = () => {
+    if (redisClient) {
+      return redisClient;
+    }
+    if (opts.cluster) {
+      redisClient = new Redis.Cluster(opts.cluster, opts.clusterOptions);
+    } else if (opts.url) {
+      redisClient = new Redis(opts.url, opts);
+    } else {
+      redisClient = new Redis(opts);
+    }
+    return redisClient;
+  };
+
+  const base = (opts.base || "").replace(/:$/, "");
+  const p = (...keys: string[]) => joinKeys(base, ...keys); // Prefix a key. Uses base for backwards compatibility
+  const d = (key: string) => (base ? key.replace(base, "") : key); // Deprefix a key
+
+  return {
+    name: DRIVER_NAME,
+    options: opts,
+    async hasItem(key) {
+      return Boolean(await getRedisClient().exists(p(key)));
+    },
+    async getItem(key) {
+      const value = await getRedisClient().get(p(key));
+      return value === null ? null : value;
+    },
+    async setItem(key, value, tOptions) {
+      let ttl = tOptions?.ttl ?? opts.ttl;
+      if (ttl) {
+        await getRedisClient().set(p(key), value, "EX", ttl);
+      } else {
+        await getRedisClient().set(p(key), value);
+      }
+    },
+    async removeItem(key) {
+      await getRedisClient().del(p(key));
+    },
+    async getKeys(base) {
+      const keys: string[] = await getRedisClient().keys(p(base, "*"));
+      return keys.map((key) => d(key));
+    },
+    async clear(base) {
+      const keys = await getRedisClient().keys(p(base, "*"));
+      if (keys.length === 0) {
+        return;
+      }
+      return getRedisClient()
+        .del(keys)
+        .then(() => {});
+    },
+    dispose() {
+      return getRedisClient().disconnect();
+    },
+  };
+});

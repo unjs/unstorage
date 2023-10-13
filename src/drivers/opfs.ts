889df3973b9adfac545fc972e1845c301125c60f
loilo <florian.reuschel@gmx.de>
2023-10-13 13:25:54 +0200

    fix(opfs): lazily access the OPFS root and throw proper error if not available

--- src/drivers/opfs.ts
+++ src/drivers/opfs.ts
@@ -1,4 +1,4 @@
-import { defineDriver } from "./utils";
+import { defineDriver, createError } from "./utils";
 import {
   DRIVER_NAME,
   exists,
@@ -32,32 +32,45 @@ export interface OPFSStorageOptions {
   ignore?: (path: string) => boolean;
 }
 
+let defaultFsHandle: Promise<FileSystemDirectoryHandle> | undefined;
+async function getDefaultFs() {
+  // Use memoized OPFS handle if available
+  if (typeof defaultFsHandle !== "undefined") return defaultFsHandle;
+
+  // If no file system is provided, OPFS needs to be available
+  if (typeof globalThis?.navigator?.storage !== "object") {
+    throw createError(
+      DRIVER_NAME,
+      "No filesystem provided and navigator.storage is not available"
+    );
+  }
+
+  defaultFsHandle = navigator.storage.getDirectory();
+  return defaultFsHandle;
+}
+
 export default defineDriver<OPFSStorageOptions | undefined>(
   (opts: OPFSStorageOptions = {}) => {
-    const fsPromise = Promise.resolve(
-      opts.fs ?? navigator.storage.getDirectory()
-    );
-
     opts.base = normalizePath(opts.base ?? "");
-
+    const getFs = () => opts.fs ?? getDefaultFs();
     const resolve = (path: string) => joinPaths(opts.base!, path);
 
     return {
       name: DRIVER_NAME,
       options: opts,
       async hasItem(key) {
-        return exists(await fsPromise, resolve(key), "file");
+        return exists(await getFs(), resolve(key), "file");
       },
       async getItem(key) {
-        if (!(await exists(await fsPromise, resolve(key), "file"))) return null;
-        return readFile(await fsPromise, resolve(key), "utf8");
+        if (!(await exists(await getFs(), resolve(key), "file"))) return null;
+        return readFile(await getFs(), resolve(key), "utf8");
       },
       async getItemRaw(key) {
-        if (!(await exists(await fsPromise, resolve(key), "file"))) return null;
-        return readFile(await fsPromise, resolve(key));
+        if (!(await exists(await getFs(), resolve(key), "file"))) return null;
+        return readFile(await getFs(), resolve(key));
       },
       async getMeta(key) {
-        const file = await getFileObject(await fsPromise, resolve(key));
+        const file = await getFileObject(await getFs(), resolve(key));
         if (!file) return null;
 
         return {
@@ -67,23 +80,23 @@ export default defineDriver<OPFSStorageOptions | undefined>(
         };
       },
       async setItem(key, value) {
-        return writeFile(await fsPromise, resolve(key), value);
+        return writeFile(await getFs(), resolve(key), value);
       },
       async setItemRaw(key, value) {
-        return writeFile(await fsPromise, resolve(key), value);
+        return writeFile(await getFs(), resolve(key), value);
       },
       async removeItem(key) {
-        return unlink(await fsPromise, resolve(key));
+        return unlink(await getFs(), resolve(key));
       },
       async getKeys() {
-        return readdirRecursive(await fsPromise, resolve(""), opts.ignore);
+        return readdirRecursive(await getFs(), resolve(""), opts.ignore);
       },
       async clear() {
         if (opts.base!.length === 0) {
           // We cannot delete an OPFS root, so we just empty it
-          await removeChildren(await fsPromise, resolve(""));
+          await removeChildren(await getFs(), resolve(""));
         } else {
-          await remove(await fsPromise, resolve(""));
+          await remove(await getFs(), resolve(""));
         }
       },
     };

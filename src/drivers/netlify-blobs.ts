50cdd11215ca9f97ff2798afb32fd9490bb0cb2f
Pooya Parsa <pooya@pi0.io>
2023-11-14 18:10:52 +0100

    add jdocs and export types

--- src/drivers/netlify-blobs.ts
+++ src/drivers/netlify-blobs.ts
@@ -9,24 +9,30 @@ import { fetch } from "ofetch";
 
 const DRIVER_NAME = "netlify-blobs";
 
-interface NetlifyBaseStoreOptions {
-  deployScoped?: boolean;
+export interface NetlifyBaseStoreOptions {
+  /** The name of the store to use. It is created if needed. This is required except for deploy-scoped stores. */
   name?: string;
-  apiURL?: string;
-  edgeURL?: string;
+  /** If set to `true`, the store is scoped to the deploy. This means that it is only available from that deploy, and will be deleted or rolled-back alongside it. */
+  deployScoped?: boolean;
+  /** Required during builds, where it is available as `constants.SITE_ID`. At runtime this is set automatically. */
   siteID?: string;
+  /** Required during builds, where it is available as `constants.NETLIFY_API_TOKEN`. At runtime this is set automatically. */
   token?: string;
+  /** Used for advanced use cases and unit tests */
+  apiURL?: string;
+  /** Used for advanced use cases and unit tests */
+  edgeURL?: string;
 }
 
-interface NetlifyDeployStoreOptions extends NetlifyBaseStoreOptions {
-  deployScoped: true;
+export interface NetlifyDeployStoreOptions extends NetlifyBaseStoreOptions {
   name?: never;
+  deployScoped: true;
   deployID?: string;
 }
 
-interface NetlifyNamedStoreOptions extends NetlifyBaseStoreOptions {
-  deployScoped?: false;
+export interface NetlifyNamedStoreOptions extends NetlifyBaseStoreOptions {
   name: string;
+  deployScoped?: false;
 }
 
 export type NetlifyStoreOptions =

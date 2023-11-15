21e83ffd129feca1b9eacfe100022c6761f734dc
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-11-16 00:44:47 +0400

    Refactor web storage driver to use shared code

--- src/drivers/localstorage.ts
+++ src/drivers/localstorage.ts
@@ -1,4 +1,5 @@
 import { createRequiredError, defineDriver } from "./utils";
+import webStorage from "./web-storage";
 
 export interface LocalStorageOptions {
   base?: string;
@@ -19,57 +20,9 @@ export default defineDriver((opts: LocalStorageOptions = {}) => {
     throw createRequiredError(DRIVER_NAME, "localStorage");
   }
 
-  const r = (key: string) => (opts.base ? opts.base + ":" : "") + key;
-
-  let _storageListener: undefined | ((ev: StorageEvent) => void);
-  const _unwatch = () => {
-    if (_storageListener) {
-      opts.window?.removeEventListener("storage", _storageListener);
-    }
-    _storageListener = undefined;
-  };
-
-  return {
-    name: DRIVER_NAME,
-    options: opts,
-    hasItem(key) {
-      return Object.prototype.hasOwnProperty.call(opts.localStorage!, r(key));
-    },
-    getItem(key) {
-      return opts.localStorage!.getItem(r(key));
-    },
-    setItem(key, value) {
-      return opts.localStorage!.setItem(r(key), value);
-    },
-    removeItem(key) {
-      return opts.localStorage!.removeItem(r(key));
-    },
-    getKeys() {
-      return Object.keys(opts.localStorage!);
-    },
-    clear() {
-      if (!opts.base) {
-        opts.localStorage!.clear();
-      } else {
-        for (const key of Object.keys(opts.localStorage!)) {
-          opts.localStorage?.removeItem(key);
-        }
-      }
-      if (opts.window && _storageListener) {
-        opts.window.removeEventListener("storage", _storageListener);
-      }
-    },
-    watch(callback) {
-      if (!opts.window) {
-        return _unwatch;
-      }
-      _storageListener = (ev: StorageEvent) => {
-        if (ev.key) {
-          callback(ev.newValue ? "update" : "remove", ev.key);
-        }
-      };
-      opts.window.addEventListener("storage", _storageListener);
-      return _unwatch;
-    },
-  };
+  return webStorage({
+    base: opts.base,
+    window: opts.window,
+    storageArea: opts.localStorage,
+  })
 });

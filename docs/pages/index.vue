e1630c298e65d2186b7517bde7ace2ca4bafe3cf
Florent Delerue <florentdelerue@hotmail.com>
2023-11-20 12:10:54 +0100

    up

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 const navigation = inject('navigation')
 
-definePageMeta({
-  colorMode: 'dark',
-})
-
 const title = 'Unstorage'
 const description = 'Unstorage provides an async Key-Value storage API with conventional features like multi driver mounting, watching and working with metadata, dozens of built-in drivers and a tiny core.'
 
@@ -14,11 +10,7 @@ useSeoMeta({
   ogTitle: title,
   description,
   ogDescription: description,
-  // ogImage: 'https://devtools.nuxt.com/social-card.png',
-  // twitterImage: 'https://devtools.nuxt.com/social-card.png',
 })
-
-console.log('navigation', navigation)
 </script>
 
 <template>

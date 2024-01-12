2287b3e31e467a99d242b71381c52e4d1f10d247
Pooya Parsa <pooya@pi0.io>
2024-01-12 13:06:09 +0100

    reduce diff

--- src/drivers/cloudflare-r2-binding.ts
+++ src/drivers/cloudflare-r2-binding.ts
@@ -11,78 +11,75 @@ export interface CloudflareR2Options {
 
 const DRIVER_NAME = "cloudflare-r2-binding";
 
-export default defineDriver(
-  (_opts: CloudflareR2Options = { binding: "BUCKET" }) => {
-    const opts: CloudflareR2Options = {
-      binding: "BUCKET",
-      ..._opts,
-    };
+export default defineDriver((_opts: CloudflareR2Options = {}) => {
+  const opts: CloudflareR2Options = {
+    binding: "BUCKET",
+    ..._opts,
+  };
 
-    const r = (key: string = "") =>
-      opts.base ? joinKeys(opts.base, key) : key;
+  const r = (key: string = "") => (opts.base ? joinKeys(opts.base, key) : key);
 
-    const getKeys = async (base?: string) => {
+  const getKeys = async (base?: string) => {
+    const binding = getBinding(opts.binding!);
+    const kvList = await binding.list(
+      base || opts.base ? { prefix: r(base) } : undefined
+    );
+    return kvList.objects.map((obj) => obj.key);
+  };
+
+  return {
+    name: DRIVER_NAME,
+    options: opts,
+    async hasItem(key) {
+      key = r(key);
       const binding = getBinding(opts.binding!);
-      const kvList = await binding.list(
-        base || opts.base ? { prefix: r(base) } : undefined
+      return (await binding.head(key)) !== null;
+    },
+    async getMeta(key, topts) {
+      key = r(key);
+      const binding = getBinding(opts.binding!);
+      const obj = await binding.head(key);
+      if (!obj) return null;
+      return {
+        mtime: obj.uploaded,
+        atime: obj.uploaded,
+        ...obj,
+      };
+    },
+    getItem(key, topts) {
+      key = r(key);
+      const binding = getBinding(opts.binding!);
+      return binding.get(key, topts).then((r) => r?.text());
+    },
+    getItemRaw(key, topts) {
+      key = r(key);
+      const binding = getBinding(opts.binding!);
+      return binding.get(key, topts).then((r) => r?.arrayBuffer());
+    },
+    async setItem(key, value, topts) {
+      key = r(key);
+      const binding = getBinding(opts.binding!);
+      await binding.put(key, value, topts);
+    },
+    async setItemRaw(key, value, topts) {
+      key = r(key);
+      const binding = getBinding(opts.binding!);
+      await binding.put(key, value, topts);
+    },
+    async removeItem(key) {
+      key = r(key);
+      const binding = getBinding(opts.binding!);
+      await binding.delete(key);
+    },
+    getKeys(base) {
+      return getKeys(base).then((keys) =>
+        opts.base ? keys.map((key) => key.slice(opts.base!.length)) : keys
       );
-      return kvList.objects.map((obj) => obj.key);
-    };
-
-    return {
-      name: DRIVER_NAME,
-      options: opts,
-      async hasItem(key) {
-        key = r(key);
-        const binding = getBinding(opts.binding!);
-        return (await binding.head(key)) !== null;
-      },
-      async getMeta(key, topts) {
-        key = r(key);
-        const binding = getBinding(opts.binding!);
-        const obj = await binding.head(key);
-        if (!obj) return null;
-        return {
-          mtime: obj.uploaded,
-          atime: obj.uploaded,
-          ...obj,
-        };
-      },
-      getItem(key, topts) {
-        key = r(key);
-        const binding = getBinding(opts.binding!);
-        return binding.get(key, topts).then((r) => r?.text());
-      },
-      getItemRaw(key, topts) {
-        key = r(key);
-        const binding = getBinding(opts.binding!);
-        return binding.get(key, topts).then((r) => r?.arrayBuffer());
-      },
-      async setItem(key, value, topts) {
-        key = r(key);
-        const binding = getBinding(opts.binding!);
-        await binding.put(key, value, topts);
-      },
-      async setItemRaw(key, value, topts) {
-        key = r(key);
-        const binding = getBinding(opts.binding!);
-        await binding.put(key, value, topts);
-      },
-      async removeItem(key) {
-        key = r(key);
-        const binding = getBinding(opts.binding!);
-        await binding.delete(key);
-      },
-      getKeys(base) {
-        return getKeys(base).then((keys) =>
-          opts.base ? keys.map((key) => key.slice(opts.base!.length)) : keys
-        );
-      },
-      async clear(base) {
-        const binding = getBinding(opts.binding!);
-        const keys = await getKeys(base);
-        await binding.delete(keys);
-      },
-    };
-  }
-);
+    },
+    async clear(base) {
+      const binding = getBinding(opts.binding!);
+      const keys = await getKeys(base);
+      await binding.delete(keys);
+    },
+  };
+});

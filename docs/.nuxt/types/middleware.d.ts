22685b1c8849fee859e7460ccb76d74eab478161
Sébastien Chopin <seb@nuxt.com>
2024-01-12 16:49:55 +0100

    chore: up

--- docs/.nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "../../node_modules/.pnpm/nuxt@3.9.1_eslint@8.56.0_typescript@5.3.3_vite@5.0.11_vue-tsc@1.8.27/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
--- docs/.nuxt/ui.colors.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '#ui-colors' { const defaultExport: ["red","orange","amber","yellow","lime","green","emerald","teal","cyan","sky","blue","indigo","violet","purple","fuchsia","pink","rose","theme","primary"]; export default defaultExport; }

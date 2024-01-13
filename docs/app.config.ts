d5747c144895c1eb9f09182fde146891888dfdce
Pooya Parsa <pooya@pi0.io>
2024-01-13 02:28:58 +0100

    refactor: switch to unjs-docs

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,6 +1,9 @@
-export default defineAppConfig({
-  name: 'Unstorage',
-  description: 'A simple, small, and fast key-value storage library for JavaScript.',
-  github: 'unjs/unstorage',
-  // logo: 'https://unjs.io/assets/logos/unstorage.svg'
-})
+export default {
+  docs: {
+    name: "Unstorage",
+    description:
+      "A simple, small, and fast key-value storage library for JavaScript.",
+    github: "unjs/unstorage",
+    // logo: "https://unjs.io/assets/logos/unstorage.svg",
+  },
+};

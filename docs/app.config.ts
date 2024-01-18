e09c13d56eb28dcfed40987b4a0174d54265be1b
Pooya Parsa <pooya@pi0.io>
2024-01-18 17:28:40 +0100

    update

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -4,9 +4,8 @@ export default {
     description:
       "A simple, small, and fast key-value storage library for JavaScript.",
     github: "unjs/unstorage",
-    // logo: "https://unjs.io/assets/logos/unstorage.svg",
     socials: {
-      github: 'unjs/unstorage'
-    }
+      github: "unjs/unstorage",
+    },
   },
 };

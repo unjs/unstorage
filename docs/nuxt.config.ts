f85112fae3e13eafee5d5d57a12fab66d73c9994
Pooya Parsa <pooya@pi0.io>
2024-01-24 01:56:09 +0100

    chore: update docs

--- docs/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtConfig({
-  routeRules: {
-    '/usage': { redirect: '/getting-started/usage' },
-    '/utils': { redirect: '/getting-started/utils' },
-    '/http-server': { redirect: '/getting-started/http-server' },
-    '/custom-driver': { redirect: '/getting-started/custom-driver' },
-  }
-})

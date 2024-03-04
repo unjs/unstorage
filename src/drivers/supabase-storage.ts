ba3e19b16bc4d58291738c23cd99461876fcdc2b
PikiLee <871565198@qq.com>
2024-03-04 16:24:59 +0800

    feat: add supabase storage

---
+++ src/drivers/supabase-storage.ts
@@ -0,0 +1,126 @@
+import { SupabaseClient, createClient } from "@supabase/supabase-js";
+import { createError, defineDriver, joinKeys, normalizeKey } from "./utils";
+
+export interface SupabaseOptions {
+  base?: string;
+  url?: string;
+  key?: string;
+  bucket?: string;
+}
+
+const DRIVER_NAME = "supabase-storage";
+
+export default defineDriver((opts: SupabaseOptions) => {
+  if (!opts.url) {
+    throw createError(DRIVER_NAME, "url");
+  }
+  if (!opts.key) {
+    throw createError(DRIVER_NAME, "key");
+  }
+  if (!opts.bucket) {
+    throw createError(DRIVER_NAME, "bucket");
+  }
+
+  const r = (key: string = "") => {
+    return (opts.base ? joinKeys(opts.base, key) : normalizeKey(key)).replace(
+      /:/g,
+      "/"
+    );
+  };
+
+  let client: SupabaseClient;
+
+  const getClient = () => {
+    if (!client) {
+      client = createClient(opts.url!, opts.key!);
+    }
+    return client;
+  };
+
+  const _getKeys = async (prefix: string): Promise<string[]> => {
+    const { data, error } = await getClient()
+      .storage.from(opts.bucket!)
+      .list(prefix);
+
+    if (error) throw error;
+    if (!data) return [];
+
+    const keys: string[] = [];
+    for (const { name, id } of data) {
+      const key = `${prefix !== "" ? prefix + "/" : ""}${name}`;
+      if (!id) {
+        keys.push(...(await _getKeys(key)));
+      } else {
+        keys.push(key);
+      }
+    }
+    return keys;
+  };
+
+  return {
+    name: DRIVER_NAME,
+    options: opts,
+    async hasItem(key) {
+      const segments = r(key).split("/");
+      const prefix = segments.slice(0, -1).join("/");
+      const name = segments.at(-1);
+      const { data, error } = await getClient()
+        .storage.from(opts.bucket!)
+        .list(prefix, {
+          search: name,
+        });
+
+      if (error) throw error;
+      return data.length > 0 && !!data[0].id;
+    },
+    async getItem(key) {
+      const { data, error } = await getClient()
+        .storage.from(opts.bucket!)
+        .download(r(key));
+
+      if (error) return null;
+      return await data.text();
+    },
+    async getItemRaw(key) {
+      const { data, error } = await getClient()
+        .storage.from(opts.bucket!)
+        .download(r(key));
+
+      if (error) return null;
+      return data.arrayBuffer();
+    },
+    async setItem(key, value) {
+      const { error } = await getClient()
+        .storage.from(opts.bucket!)
+        .upload(r(key), value, {
+          upsert: true,
+        });
+
+      if (error) throw error;
+    },
+    async setItemRaw(key, value) {
+      const { error } = await getClient()
+        .storage.from(opts.bucket!)
+        .upload(r(key), value, {
+          upsert: true,
+        });
+
+      if (error) throw error;
+    },
+    async removeItem(key) {
+      const { error } = await getClient()
+        .storage.from(opts.bucket!)
+        .remove([r(key)]);
+
+      if (error) throw error;
+    },
+    async getKeys(base) {
+      const keys = await _getKeys(r(base));
+      return opts.base ? keys.map((key) => key.slice(opts.base!.length)) : keys;
+    },
+    async clear(base) {
+      const keys = await _getKeys(r(base));
+      await getClient().storage.from(opts.bucket!).remove(keys);
+    },
+  };
+});
---
+++ test/drivers/supabase-storage.test.ts
@@ -0,0 +1,142 @@
+import { describe, vi } from "vitest";
+import driver from "../../src/drivers/supabase-storage";
+import { testDriver } from "./utils";
+import { createStorage } from "../../src";
+import { FileObject, StorageClient, StorageError } from "@supabase/storage-js";
+import { joinKeys, normalizeKey } from "../../src/drivers/utils";
+
+type StorageFileApi = ReturnType<StorageClient["from"]>;
+
+const mockStorage = createStorage();
+
+const mockFileObject = (isFile: boolean, name: string): FileObject => ({
+  name: name,
+  id: isFile ? Math.random().toString() : "",
+  bucket_id: "",
+  owner: "",
+  updated_at: "",
+  created_at: "",
+  last_accessed_at: "",
+  metadata: {},
+  buckets: {
+    id: "",
+    name: "",
+    owner: "",
+    created_at: "",
+    updated_at: "",
+    public: false,
+  },
+});
+
+vi.mock("@supabase/supabase-js", () => {
+  const list: StorageFileApi["list"] = async (path, options) => {
+    try {
+      const allKeys = await mockStorage.getKeys();
+
+      // Find the exact file or folder
+      if (options?.search) {
+        const key = joinKeys(path || "", options.search || "");
+        const data = allKeys.includes(key)
+          ? [mockFileObject(true, key)]
+          : allKeys.some((k) => k.startsWith(key))
+            ? [mockFileObject(false, key)]
+            : [];
+        return {
+          data,
+          error: null,
+        };
+      }
+
+      // List all files and folders under the path
+      const key = normalizeKey(path || "");
+      const keys = allKeys
+        .filter((k) => k.startsWith(key))
+        .map((k) => normalizeKey(k.replace(key, "")));
+      return {
+        data: keys.map((key) => {
+          const isFile = /^[^:]+$/.test(key);
+          const segments = key.split(":");
+          return mockFileObject(isFile, segments[0]);
+        }),
+        error: null,
+      };
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const upload: StorageFileApi["upload"] = async (path, fileBody) => {
+    try {
+      await mockStorage.setItemRaw(path, fileBody);
+      return {
+        data: { path },
+        error: null,
+      };
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const remove: StorageFileApi["remove"] = async (paths) => {
+    try {
+      await Promise.all(paths.map((p) => mockStorage.removeItem(p)));
+      return {
+        data: paths.map((path) =>
+          mockFileObject(true, path.split("/").at(-1) || "")
+        ),
+        error: null,
+      };
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const download: StorageFileApi["download"] = async (path) => {
+    try {
+      const file = await mockStorage.getItemRaw(path);
+      return file
+        ? {
+            data: new Blob([file]),
+            error: null,
+          }
+        : {
+            data: null,
+            error: new StorageError(""),
+          };
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return {
+    createClient: () => ({
+      storage: {
+        from: () => ({
+          list,
+          upload,
+          remove,
+          download,
+        }),
+      },
+    }),
+  };
+});
+
+describe("drivers: supabase-storage", async () => {
+  testDriver({
+    driver: driver({
+      url: "<your Supabase project URL>",
+      key: "<your Supabase project API key>",
+      bucket: "<your Supabase project storage bucket name>",
+    }),
+  });
+
+  testDriver({
+    driver: driver({
+      base: "output",
+      url: "<your Supabase project URL>",
+      key: "<your Supabase project API key>",
+      bucket: "<your Supabase project storage bucket name>",
+    }),
+  });
+});

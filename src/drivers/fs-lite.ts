822c47ae020826eb167c1c04465cf487777848b8
productdevbook <hi@productdevbook.com>
2024-03-25 16:25:10 +0300

    fix: lint

--- src/drivers/fs-lite.ts
+++ src/drivers/fs-lite.ts
@@ -25,73 +25,69 @@ const PATH_TRAVERSE_RE = /\.\.\:|\.\.$/;
 
 const DRIVER_NAME = "fs-lite";
 
-export default defineDriver(
-  (
-    opts: FSStorageOptions = {
-      baseResolve: true,
-    }
-  ) => {
-    if (!opts.base) {
-      throw createRequiredError(DRIVER_NAME, "base");
-    }
-
-    opts.base = opts.baseResolve ? resolve(opts.base) : opts.base;
-    const r = (key: string) => {
-      if (PATH_TRAVERSE_RE.test(key)) {
-        throw createError(
-          DRIVER_NAME,
-          `Invalid key: ${JSON.stringify(key)}. It should not contain .. segments`
-        );
-      }
-      const resolved = join(opts.base!, key.replace(/:/g, "/"));
-      return resolved;
-    };
-
-    return {
-      name: DRIVER_NAME,
-      options: opts,
-      hasItem(key) {
-        return existsSync(r(key));
-      },
-      getItem(key) {
-        return readFile(r(key), "utf8");
-      },
-      getItemRaw(key) {
-        return readFile(r(key));
-      },
-      async getMeta(key) {
-        const { atime, mtime, size, birthtime, ctime } = await fsp
-          .stat(r(key))
-          .catch(() => ({}) as Stats);
-        return { atime, mtime, size, birthtime, ctime };
-      },
-      setItem(key, value) {
-        if (opts.readOnly) {
-          return;
-        }
-        return writeFile(r(key), value, "utf8");
-      },
-      setItemRaw(key, value) {
-        if (opts.readOnly) {
-          return;
-        }
-        return writeFile(r(key), value);
-      },
-      removeItem(key) {
-        if (opts.readOnly) {
-          return;
-        }
-        return unlink(r(key));
-      },
-      getKeys() {
-        return readdirRecursive(r("."), opts.ignore);
-      },
-      async clear() {
-        if (opts.readOnly || opts.noClear) {
-          return;
-        }
-        await rmRecursive(r("."));
-      },
-    };
+export default defineDriver((opts: FSStorageOptions = {
+  baseResolve: true
+}) => {
+  if (!opts.base) {
+    throw createRequiredError(DRIVER_NAME, "base");
   }
-);
+
+  opts.base = opts.baseResolve ? resolve(opts.base) : opts.base;
+  const r = (key: string) => {
+    if (PATH_TRAVERSE_RE.test(key)) {
+      throw createError(
+        DRIVER_NAME,
+        `Invalid key: ${JSON.stringify(key)}. It should not contain .. segments`
+      );
+    }
+    const resolved = join(opts.base!, key.replace(/:/g, "/"));
+    return resolved;
+  };
+
+  return {
+    name: DRIVER_NAME,
+    options: opts,
+    hasItem(key) {
+      return existsSync(r(key));
+    },
+    getItem(key) {
+      return readFile(r(key), "utf8");
+    },
+    getItemRaw(key) {
+      return readFile(r(key));
+    },
+    async getMeta(key) {
+      const { atime, mtime, size, birthtime, ctime } = await fsp
+        .stat(r(key))
+        .catch(() => ({}) as Stats);
+      return { atime, mtime, size, birthtime, ctime };
+    },
+    setItem(key, value) {
+      if (opts.readOnly) {
+        return;
+      }
+      return writeFile(r(key), value, "utf8");
+    },
+    setItemRaw(key, value) {
+      if (opts.readOnly) {
+        return;
+      }
+      return writeFile(r(key), value);
+    },
+    removeItem(key) {
+      if (opts.readOnly) {
+        return;
+      }
+      return unlink(r(key));
+    },
+    getKeys() {
+      return readdirRecursive(r("."), opts.ignore);
+    },
+    async clear() {
+      if (opts.readOnly || opts.noClear) {
+        return;
+      }
+      await rmRecursive(r("."));
+    },
+  };
+});
--- src/drivers/fs.ts
+++ src/drivers/fs.ts
@@ -27,115 +27,111 @@ const PATH_TRAVERSE_RE = /\.\.\:|\.\.$/;
 
 const DRIVER_NAME = "fs";
 
-export default defineDriver(
-  (
-    opts: FSStorageOptions = {
-      baseResolve: true,
-    }
-  ) => {
-    if (!opts.base) {
-      throw createRequiredError(DRIVER_NAME, "base");
-    }
+export default defineDriver((opts: FSStorageOptions = {
+  baseResolve: true
+}) => {
+  if (!opts.base) {
+    throw createRequiredError(DRIVER_NAME, "base");
+  }
 
-    if (!opts.ignore) {
-      opts.ignore = ["**/node_modules/**", "**/.git/**"];
-    }
+  if (!opts.ignore) {
+    opts.ignore = ["**/node_modules/**", "**/.git/**"];
+  }
 
-    opts.base = opts.baseResolve ? resolve(opts.base) : opts.base;
-    const r = (key: string) => {
-      if (PATH_TRAVERSE_RE.test(key)) {
-        throw createError(
-          DRIVER_NAME,
-          `Invalid key: ${JSON.stringify(key)}. It should not contain .. segments`
-        );
+  opts.base = opts.baseResolve ? resolve(opts.base) : opts.base;
+  const r = (key: string) => {
+    if (PATH_TRAVERSE_RE.test(key)) {
+      throw createError(
+        DRIVER_NAME,
+        `Invalid key: ${JSON.stringify(key)}. It should not contain .. segments`
+      );
+    }
+    const resolved = join(opts.base!, key.replace(/:/g, "/"));
+    return resolved;
+  };
+
+  let _watcher: FSWatcher | undefined;
+  const _unwatch = async () => {
+    if (_watcher) {
+      await _watcher.close();
+      _watcher = undefined;
+    }
+  };
+
+  return {
+    name: DRIVER_NAME,
+    options: opts,
+    hasItem(key) {
+      return existsSync(r(key));
+    },
+    getItem(key) {
+      return readFile(r(key), "utf8");
+    },
+    getItemRaw(key) {
+      return readFile(r(key));
+    },
+    async getMeta(key) {
+      const { atime, mtime, size, birthtime, ctime } = await fsp
+        .stat(r(key))
+        .catch(() => ({}) as Stats);
+      return { atime, mtime, size, birthtime, ctime };
+    },
+    setItem(key, value) {
+      if (opts.readOnly) {
+        return;
       }
-      const resolved = join(opts.base!, key.replace(/:/g, "/"));
-      return resolved;
-    };
-
-    let _watcher: FSWatcher | undefined;
-    const _unwatch = async () => {
+      return writeFile(r(key), value, "utf8");
+    },
+    setItemRaw(key, value) {
+      if (opts.readOnly) {
+        return;
+      }
+      return writeFile(r(key), value);
+    },
+    removeItem(key) {
+      if (opts.readOnly) {
+        return;
+      }
+      return unlink(r(key));
+    },
+    getKeys() {
+      return readdirRecursive(r("."), anymatch(opts.ignore || []));
+    },
+    async clear() {
+      if (opts.readOnly || opts.noClear) {
+        return;
+      }
+      await rmRecursive(r("."));
+    },
+    async dispose() {
       if (_watcher) {
         await _watcher.close();
-        _watcher = undefined;
       }
-    };
-
-    return {
-      name: DRIVER_NAME,
-      options: opts,
-      hasItem(key) {
-        return existsSync(r(key));
-      },
-      getItem(key) {
-        return readFile(r(key), "utf8");
-      },
-      getItemRaw(key) {
-        return readFile(r(key));
-      },
-      async getMeta(key) {
-        const { atime, mtime, size, birthtime, ctime } = await fsp
-          .stat(r(key))
-          .catch(() => ({}) as Stats);
-        return { atime, mtime, size, birthtime, ctime };
-      },
-      setItem(key, value) {
-        if (opts.readOnly) {
-          return;
-        }
-        return writeFile(r(key), value, "utf8");
-      },
-      setItemRaw(key, value) {
-        if (opts.readOnly) {
-          return;
-        }
-        return writeFile(r(key), value);
-      },
-      removeItem(key) {
-        if (opts.readOnly) {
-          return;
-        }
-        return unlink(r(key));
-      },
-      getKeys() {
-        return readdirRecursive(r("."), anymatch(opts.ignore || []));
-      },
-      async clear() {
-        if (opts.readOnly || opts.noClear) {
-          return;
-        }
-        await rmRecursive(r("."));
-      },
-      async dispose() {
-        if (_watcher) {
-          await _watcher.close();
-        }
-      },
-      async watch(callback) {
-        if (_watcher) {
-          return _unwatch;
-        }
-        await new Promise<void>((resolve, reject) => {
-          _watcher = watch(opts.base!, {
-            ignoreInitial: true,
-            ignored: opts.ignore,
-            ...opts.watchOptions,
-          })
-            .on("ready", () => {
-              resolve();
-            })
-            .on("error", reject)
-            .on("all", (eventName, path) => {
-              path = relative(opts.base!, path);
-              if (eventName === "change" || eventName === "add") {
-                callback("update", path);
-              } else if (eventName === "unlink") {
-                callback("remove", path);
-              }
-            });
-        });
+    },
+    async watch(callback) {
+      if (_watcher) {
         return _unwatch;
-      },
-    };
-  }
-);
+      }
+      await new Promise<void>((resolve, reject) => {
+        _watcher = watch(opts.base!, {
+          ignoreInitial: true,
+          ignored: opts.ignore,
+          ...opts.watchOptions,
+        })
+          .on("ready", () => {
+            resolve();
+          })
+          .on("error", reject)
+          .on("all", (eventName, path) => {
+            path = relative(opts.base!, path);
+            if (eventName === "change" || eventName === "add") {
+              callback("update", path);
+            } else if (eventName === "unlink") {
+              callback("remove", path);
+            }
+          });
+      });
+      return _unwatch;
+    },
+  };
+});

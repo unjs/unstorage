f572ea7f3df754342c4ab8b2bf90ed897a7d8fa7
Michael Brevard <yonshi29@gmail.com>
2024-05-01 12:47:28 +0300

    perf(getKeys): avoid duplicate iteration (#386)
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

--- src/storage.ts
+++ src/storage.ts
@@ -340,10 +340,12 @@ export function createStorage<T extends StorageValue>(
           mount.relativeBase,
           opts
         );
-        const keys = rawKeys
-          .map((key) => mount.mountpoint + normalizeKey(key))
-          .filter((key) => !maskedMounts.some((p) => key.startsWith(p)));
-        allKeys.push(...keys);
+        for (const key of rawKeys) {
+          const fullKey = mount.mountpoint + normalizeKey(key);
+          if (!maskedMounts.some((p) => fullKey.startsWith(p))) {
+            allKeys.push(fullKey);
+          }
+        }
 
         // When /mnt/foo is processed, any key in /mnt with /mnt/foo prefix should be masked
         // Using filter to improve performance. /mnt mask already covers /mnt/foo

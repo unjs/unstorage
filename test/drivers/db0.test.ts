de22b02c22227fdf9dd33973873f4d50b19f8be4
Balázs Németh <zsilbi@zsilbi.hu>
2024-09-04 23:05:16 +0200

    test(db0): use in-memory sqlite, meta test

--- test/drivers/db0.test.ts
+++ test/drivers/db0.test.ts
@@ -1,40 +1,36 @@
 import { createDatabase } from "db0";
 import libSql from "db0/connectors/libsql/node";
 import postgresql from "db0/connectors/postgresql";
-import { existsSync, mkdirSync, unlinkSync } from "node:fs";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { afterAll, beforeAll, describe } from "vitest";
+import { afterAll, describe, expect } from "vitest";
 import db0Driver from "../../src/drivers/db0";
 import { testDriver } from "./utils";
 
 describe("drivers: db0 - libSQL", () => {
-  const dbPath = resolve(
-    dirname(fileURLToPath(import.meta.url)),
-    "tmp/db0/libsql/local.db"
-  );
-  if (existsSync(dbPath)) {
-    unlinkSync(dbPath);
-  }
-  mkdirSync(dirname(dbPath), { recursive: true });
-
   const db = createDatabase(
     libSql({
-      url: `file:${dbPath}`,
+      url: `:memory:`,
     })
   );
 
-  beforeAll(async () => {
-    await db.sql`CREATE TABLE storage (id TEXT PRIMARY KEY, value TEXT, created_at TEXT DEFAULT CURRENT_TIMESTAMP, updated_at TEXT DEFAULT CURRENT_TIMESTAMP)`;
+  afterAll(async () => {
+    await db.sql`DROP TABLE IF EXISTS unstorage`;
   });
 
   testDriver({
     driver: db0Driver({ database: db, dialect: "sqlite" }),
+    additionalTests: async (ctx) => {
+      await ctx.storage.setItem("meta:test", "test_data");
+
+      expect(await ctx.storage.getMeta("meta:test")).toMatchObject({
+        birthtime: expect.any(Date),
+        mtime: expect.any(Date),
+      });
+    },
   });
 });
 
 describe.runIf(process.env.POSTGRESQL_URL)(
-  "drivers: db0 - postgresql",
+  "drivers: db0 - PostgreSQL",
   async () => {
     const db = createDatabase(
       postgresql({
@@ -42,20 +38,24 @@ describe.runIf(process.env.POSTGRESQL_URL)(
       })
     );
 
-    beforeAll(async () => {
-      await db.sql`CREATE TABLE IF NOT EXISTS my_storage (id VARCHAR(255) NOT NULL PRIMARY KEY, value TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`;
-    });
-
     afterAll(async () => {
-      await db.sql`DROP TABLE IF EXISTS my_storage`;
+      await db.sql`DROP TABLE IF EXISTS custom_unstorage_table`;
     });
 
     testDriver({
       driver: db0Driver({
         database: db,
         dialect: "postgresql",
-        table: "my_storage",
+        table: "custom_unstorage_table",
       }),
+      additionalTests: async (ctx) => {
+        await ctx.storage.setItem("meta:test", "test_data");
+
+        expect(await ctx.storage.getMeta("meta:test")).toMatchObject({
+          birthtime: expect.any(Date),
+          mtime: expect.any(Date),
+        });
+      },
     });
   }
 );

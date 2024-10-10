eee5eba56761d512e7a9c18200894c7196d5a956
Balázs Németh <zsilbi@zsilbi.hu>
2024-10-10 22:12:49 +0200

    test(db0): use pglite instead of pg

--- test/drivers/db0.test.ts
+++ test/drivers/db0.test.ts
@@ -1,14 +1,14 @@
 import { createDatabase } from "db0";
 import libSql from "db0/connectors/libsql/node";
-import postgresql from "db0/connectors/postgresql";
+import pglite from "db0/connectors/pglite";
 import { afterAll, describe, expect } from "vitest";
 import db0Driver from "../../src/drivers/db0";
 import { testDriver } from "./utils";
 
-describe("drivers: db0 - libSQL", () => {
+describe("drivers: db0 - LibSQL", () => {
   const db = createDatabase(
     libSql({
-      url: `:memory:`,
+      url: ":memory:",
     })
   );
 
@@ -29,32 +29,25 @@ describe("drivers: db0 - libSQL", () => {
   });
 });
 
-describe.runIf(process.env.POSTGRESQL_URL)(
-  "drivers: db0 - PostgreSQL",
-  async () => {
-    const db = createDatabase(
-      postgresql({
-        url: process.env.POSTGRESQL_URL!,
-      })
-    );
+describe("drivers: db0 - PGlite", async () => {
+  const db = createDatabase(pglite());
 
-    afterAll(async () => {
-      await db.sql`DROP TABLE IF EXISTS custom_unstorage_table`;
-    });
+  afterAll(async () => {
+    await db.sql`DROP TABLE IF EXISTS custom_unstorage_table`;
+  });
 
-    testDriver({
-      driver: db0Driver({
-        database: db,
-        table: "custom_unstorage_table",
-      }),
-      additionalTests: async (ctx) => {
-        await ctx.storage.setItem("meta:test", "test_data");
+  testDriver({
+    driver: db0Driver({
+      database: db,
+      table: "custom_unstorage_table",
+    }),
+    additionalTests: async (ctx) => {
+      await ctx.storage.setItem("meta:test", "test_data");
 
-        expect(await ctx.storage.getMeta("meta:test")).toMatchObject({
-          birthtime: expect.any(Date),
-          mtime: expect.any(Date),
-        });
-      },
-    });
-  }
-);
+      expect(await ctx.storage.getMeta("meta:test")).toMatchObject({
+        birthtime: expect.any(Date),
+        mtime: expect.any(Date),
+      });
+    },
+  });
+});

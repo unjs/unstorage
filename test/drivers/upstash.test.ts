6caa7d6bb807f7a0a26245884f7ba0c405314a2c
Fahreddin Özcan <88107904+fahreddinozcan@users.noreply.github.com>
2024-12-06 15:55:58 +0300

    feat: add `upstash` driver (#500)
    Co-authored-by: Matt Kane <m@mk.gg>
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

---
+++ test/drivers/upstash.test.ts
@@ -0,0 +1,14 @@
+import { describe } from "vitest";
+import { testDriver } from "./utils";
+import driver from "../../src/drivers/upstash";
+
+const url = process.env.VITE_UPSTASH_REDIS_REST_URL;
+const token = process.env.VITE_UPSTASH_REDIS_REST_TOKEN;
+
+describe.skipIf(!url || !token)("drivers: upstash", async () => {
+  process.env.UPSTASH_REDIS_REST_URL = url;
+  process.env.UPSTASH_REDIS_REST_TOKEN = token;
+  testDriver({
+    driver: driver({}),
+  });
+});

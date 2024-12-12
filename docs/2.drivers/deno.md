9a8fe4c80ce600aac10cc0b49f141a13aa892ab6
Ray <i@mk1.io>
2024-12-12 22:29:58 +0800

    feat: add `deno-kv` driver (#233)
    Co-authored-by: Pooya Parsa <pooya@pi0.io>
    Co-authored-by: Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>

---
+++ docs/2.drivers/deno.md
@@ -0,0 +1,38 @@
+---
+icon: simple-icons:deno
+---
+
+# Deno KV
+
+> Store data in Deno KV
+
+::note{to="https://deno.com/kv"}
+Learn more about Deno KV.
+::
+
+## Usage
+
+::important
+`deno-kv` driver requires [Deno deploy](https://docs.deno.com/deploy/kv/manual/on_deploy/) or [Deno runtime](https://docs.deno.com/runtime/) with `--unstable-kv` CLI flag.
+::
+
+::note
+The driver automatically maps Unstorage keys to Deno. For example, `"test:key"` key will be mapped to `["test", "key"]` and vice versa.
+::
+
+```js
+import { createStorage } from "unstorage";
+import denoKVdriver from "unstorage/drivers/deno-kv";
+
+const storage = createStorage({
+  driver: denoKVdriver({
+    // path: ":memory:",
+    // base: "",
+  }),
+});
+```
+
+**Options:**
+
+- `path`: (optional) File system path to where you'd like to store your database, otherwise one will be created for you based on the current working directory of your script by Deno. You can pass `:memory:` for testing.
+- `base`: (optional) Prefix key added to all operations.
---
+++ src/drivers/deno-kv.ts
@@ -0,0 +1,106 @@
+import { normalizeKey } from "../utils";
+import { defineDriver, createError } from "./utils/index";
+
+// https://docs.deno.com/deploy/kv/manual/
+
+export interface DenoKvOptions {
+  base?: string;
+  path?: string;
+}
+
+const DRIVER_NAME = "deno-kv";
+
+export default defineDriver<DenoKvOptions, Promise<Deno.Kv>>(
+  (opts: DenoKvOptions = {}) => {
+    const basePrefix: Deno.KvKey = opts.base
+      ? normalizeKey(opts.base).split(":")
+      : [];
+
+    const r = (key: string = ""): Deno.KvKey =>
+      [...basePrefix, ...key.split(":")].filter(Boolean);
+
+    let _client: Promise<Deno.Kv> | undefined;
+    const getKv = async () => {
+      if (_client) {
+        return _client;
+      }
+      if (!globalThis.Deno) {
+        throw createError(
+          DRIVER_NAME,
+          "Missing global `Deno`. Are you running in Deno?"
+        );
+      }
+      if (!Deno.openKv) {
+        throw createError(
+          DRIVER_NAME,
+          "Missing `Deno.openKv`. Are you running Deno with --unstable-kv?"
+        );
+      }
+      _client = Deno.openKv(opts.path);
+      return _client;
+    };
+
+    return {
+      name: DRIVER_NAME,
+      getInstance() {
+        return getKv();
+      },
+      async hasItem(key) {
+        const kv = await getKv();
+        const value = await kv.get(r(key));
+        return !!value.value;
+      },
+      async getItem(key) {
+        const kv = await getKv();
+        const value = await kv.get(r(key));
+        return value.value;
+      },
+      async getItemRaw(key) {
+        const kv = await getKv();
+        const value = await kv.get(r(key));
+        return value.value;
+      },
+      async setItem(key, value) {
+        const kv = await getKv();
+        await kv.set(r(key), value);
+      },
+      async setItemRaw(key, value) {
+        const kv = await getKv();
+        await kv.set(r(key), value);
+      },
+      async removeItem(key) {
+        const kv = await getKv();
+        await kv.delete(r(key));
+      },
+      async getKeys(base) {
+        const kv = await getKv();
+        const keys: string[] = [];
+        for await (const entry of kv.list({ prefix: r(base) })) {
+          keys.push(
+            (basePrefix.length > 0
+              ? entry.key.slice(basePrefix.length)
+              : entry.key
+            ).join(":")
+          );
+        }
+        return keys;
+      },
+      async clear(base) {
+        const kv = await getKv();
+        const batch = kv.atomic();
+        for await (const entry of kv.list({ prefix: r(base) })) {
+          batch.delete(entry.key);
+        }
+        await batch.commit();
+      },
+      dispose() {
+        if (_client) {
+          return _client.then((kv) => {
+            kv.close();
+            _client = undefined;
+          });
+        }
+      },
+    };
+  }
+);

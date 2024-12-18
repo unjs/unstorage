ce9685e8f0a6a69b09ab95bc2d9a655d2d96ad73
Pooya Parsa <pooya@pi0.io>
2024-12-18 16:02:44 +0100

    test(s3): skip if not setup

--- test/drivers/s3.test.ts
+++ test/drivers/s3.test.ts
@@ -2,14 +2,23 @@ import { describe } from "vitest";
 import s3Driver from "../../src/drivers/s3";
 import { testDriver } from "./utils";
 
-describe("drivers: s3", () => {
+const accessKeyId = process.env.VITE_S3_ACCESS_KEY_ID;
+const secretAccessKey = process.env.VITE_S3_SECRET_ACCESS_KEY;
+const bucket = process.env.VITE_S3_BUCKET;
+const endpoint = process.env.VITE_S3_ENDPOINT;
+const region = process.env.VITE_S3_REGION;
+
+describe.skipIf(
+  !accessKeyId || !secretAccessKey || !bucket || !endpoint || !region
+)("drivers: s3", () => {
   testDriver({
-    driver: s3Driver({
-      accessKeyId: process.env.VITE_S3_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.VITE_S3_SECRET_ACCESS_KEY!,
-      bucket: process.env.VITE_S3_BUCKET!,
-      endpoint: process.env.VITE_S3_ENDPOINT!,
-      region: process.env.VITE_S3_REGION!,
-    }),
+    driver: () =>
+      s3Driver({
+        accessKeyId: accessKeyId!,
+        secretAccessKey: secretAccessKey!,
+        bucket: bucket!,
+        endpoint: endpoint!,
+        region: region!,
+      }),
   });
 });

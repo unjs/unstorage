c2e67eba59e5812bd5e9255da2f18609f8365a22
Daniel Roe <daniel@roe.dev>
2025-11-20 16:25:44 +0000

    build: create d.cts and d.mts type declarations

--- build.config.ts
+++ build.config.ts
@@ -1,4 +1,7 @@
 import { defineBuildConfig } from "unbuild";
+import { promises as fs } from "node:fs";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
 
 export default defineBuildConfig({
   declaration: true,
@@ -22,4 +25,26 @@ export default defineBuildConfig({
     },
   ],
   externals: ["mongodb", "unstorage", /unstorage\/drivers\//],
+  hooks: {
+    async "mkdist:done"() {
+      const driversDir = fileURLToPath(new URL("drivers", import.meta.url));
+      await scan(driversDir);
+    },
+  },
 });
+
+const scan = async (dir: string) => {
+  const files = await fs.readdir(dir);
+  for (const file of files) {
+    const filePath = join(dir, file);
+    const stats = await fs.stat(filePath);
+    if (stats.isDirectory()) {
+      await scan(filePath);
+    } else if (file.endsWith(".d.ts")) {
+      const content = await fs.readFile(filePath, "utf8");
+      await fs.writeFile(filePath.replace(".d.ts", ".d.cts"), content);
+      await fs.writeFile(filePath.replace(".d.ts", ".d.mts"), content);
+      await fs.unlink(filePath);
+    }
+  }
+};

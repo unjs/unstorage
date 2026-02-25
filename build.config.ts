92aefab92d9a0aaeab80ac1d107c3fb4914ed43a
Pooya Parsa <pooya@pi0.io>
2026-02-25 14:16:14 +0100

    build: improve dist

--- build.config.ts
+++ build.config.ts
@@ -2,9 +2,25 @@ import { defineBuildConfig } from "obuild/config";
 
 export default defineBuildConfig({
   entries: [
-    "src/index",
-    "src/server",
-    "src/tracing",
+    {
+      "type": "bundle",
+      input: [
+        "src/index.ts",
+        "src/server.ts",
+        "src/tracing.ts"
+      ],
+      rolldown: {
+        resolve: {
+          alias: {
+            "h3": "h3/generic",
+            "srvx": "srvx/generic"
+          }
+        },
+        external: [
+          /unstorage\/drivers\//,
+        ]
+      }
+    },
     {
       type: "transform",
       input: "src/drivers/",
